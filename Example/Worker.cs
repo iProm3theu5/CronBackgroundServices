da860f781d1117f0472e7585578701cc865488c5
iprom3theu5 <dave.sekula@gmail.com>
2019-09-25 21:40:11 +0100

    Added Example

---
+++ Example/Worker.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Prom3theu5.CronBasedHostedServices;
+
+namespace Example
+{
+    public class Worker : ScheduledProcessor
+    {
+        protected override string Schedule => CronTab.Minutely();
+
+        public Worker(IServiceScopeFactory serviceScopeFactory) : base(serviceScopeFactory)
+        {
+        }
+
+        public override Task ProcessInScope(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                ILogger<Worker> logger = scope.ServiceProvider.GetRequiredService <ILogger<Worker>>();
+                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
